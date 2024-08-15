6f11a7ecc1c4fdaa12f9e362dc785b5362eb1a2d
RichardT15 <134815093+RichardTham1@users.noreply.github.com>
2024-08-15 15:13:07 -0400

    load geojson

--- src/services/country-verification/country-verification.service.ts
+++ src/services/country-verification/country-verification.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { FeatureCollection, Feature } from 'geojson';
+import { environment } from '../../env/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class CountryVerificationService {
-
-  constructor() { }
+  private geojsonData: FeatureCollection | null = null;
+  constructor() {
+    this.loadGeojson();
+  }
+  private loadGeojson() {
+    fetch(`${environment.dev}/countries.geojson`)
+    .then(response => {
+      //console.log(response.json());
+      return response.json();
+    })
+    .then((data: FeatureCollection) => {
+      this.geojsonData = data;
+    })
+    .catch(err => console.error('Failed to load GeoJSON', err));
+  }
 }
