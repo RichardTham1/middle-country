d1ae390e086fd49af083ee08620e14fe0ec221c8
RichardT15 <134815093+RichardTham1@users.noreply.github.com>
2024-08-15 15:12:52 -0400

    injected countryverification service to component

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CountryVerificationService } from '../../services/country-verification/country-verification.service';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(countryVerificationService: CountryVerificationService) {}
 }
