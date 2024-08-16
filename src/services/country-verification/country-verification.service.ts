7bd11f65c78fcbcdf9b3d4c97bed4a14ab0a415e
RichardT15 <134815093+RichardTham1@users.noreply.github.com>
2024-08-15 23:28:38 -0400

    added getRandomNumber and getTwoCountries

--- src/services/country-verification/country-verification.service.ts
+++ src/services/country-verification/country-verification.service.ts
@@ -1,23 +1,47 @@
 import { Injectable } from '@angular/core';
 import { FeatureCollection, Feature } from 'geojson';
 import { environment } from '../../env/environment';
+import { CountryPair } from '../../interfaces/country-pair';
 @Injectable({
   providedIn: 'root'
 })
 export class CountryVerificationService {
   private geojsonData: FeatureCollection | null = null;
+  allCountries: string[] = [];
   constructor() {
     this.loadGeojson();
   }
-  private loadGeojson() {
-    fetch(`${environment.dev}/countries.geojson`)
-    .then(response => {
-      //console.log(response.json());
-      return response.json();
-    })
+  private loadGeojson(): void {
+    fetch(`assets/countries.geojson`)
+    .then(response => response.json())
     .then((data: FeatureCollection) => {
       this.geojsonData = data;
+    }).finally(() => {
+      this.loadCountries();
     })
     .catch(err => console.error('Failed to load GeoJSON', err));
+  }
+  loadCountries(): void {
+    this.geojsonData?.features.forEach((feature: Feature) => {
+      this.allCountries.push(feature.properties!['name']);
+    })
+    console.log(this.allCountries);
+  }
+
+  getRandomNumber(): number {
+    return Math.floor(Math.random() * this.allCountries.length);
+  }
+
+  getTwoCountries(): CountryPair {
+    let firstCountryIdx: number = this.getRandomNumber();
+    let secondCountryIdx: number = this.getRandomNumber();
+    while (secondCountryIdx == firstCountryIdx) {
+      secondCountryIdx = this.getRandomNumber();
+    }
+    return {firstCountry: this.allCountries[firstCountryIdx], secondCountry: this.allCountries[secondCountryIdx]};
+  }
+
+  verifyCountry() {
+    
   }
 }
