284e3bc22449a98ea120db61a6d5ab06139de3f3
RichardT15 <134815093+RichardTham1@users.noreply.github.com>
2024-08-16 11:48:42 -0400

    implemented get two countries and added verify answer

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CountryVerificationService } from '../../services/country-verification/country-verification.service';
+import { CountryPair } from '../../interfaces/country-pair';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,14 @@ import { CountryVerificationService } from '../../services/country-verification/
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  constructor(countryVerificationService: CountryVerificationService) {}
+  twoCountries: CountryPair | null = null;
+  constructor(public countryVerificationService: CountryVerificationService) {}
+
+  getTwoCountries(): void {
+    this.twoCountries = this.countryVerificationService.getTwoCountries();
+  }
+
+  verifyAnswer() {
+
+  }
 }
