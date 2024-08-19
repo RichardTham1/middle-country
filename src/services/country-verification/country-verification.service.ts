526e1cf0e59cd31906f073ebc2d1513b1169d688
RichardT15 <134815093+RichardTham1@users.noreply.github.com>
2024-08-19 18:16:01 -0400

    fixed get country and started verif

--- src/services/country-verification/country-verification.service.ts
+++ src/services/country-verification/country-verification.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
-import { FeatureCollection, Feature } from 'geojson';
+import { FeatureCollection, Feature, Point } from 'geojson';
 import { environment } from '../../env/environment';
 import { CountryPair } from '../../interfaces/country-pair';
+import { CSVRow } from '../../interfaces/CSVRow';
+import * as Papa from 'papaparse';
+import * as turf from '@turf/turf';
+import { LongLat } from '../../interfaces/longitude-latitude';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CountryVerificationService {
   private geojsonData: FeatureCollection | null = null;
+  private initialized: Promise<void>;
+  private countryLongLatData: Map<string, LongLat> = new Map(); 
   allCountries: string[] = [];
   constructor() {
-    this.loadGeojson();
+    this.initialized = this.initializeService();
   }
-  private loadGeojson(): void {
-    fetch(`assets/countries.geojson`)
+
+  onInitialized(): Promise<void> {
+    return this.initialized;
+  }
+
+  private async initializeService(): Promise<void> {
+    return this.loadGeojson().then(() => {
+      this.loadLongLat();
+    });
+  }
+
+  private async loadGeojson(): Promise<void> {
+    return fetch(`assets/countries.geojson`)
     .then(response => response.json())
     .then((data: FeatureCollection) => {
       this.geojsonData = data;
@@ -21,6 +39,7 @@ export class CountryVerificationService {
     })
     .catch(err => console.error('Failed to load GeoJSON', err));
   }
+
   loadCountries(): void {
     this.geojsonData?.features.forEach((feature: Feature) => {
       this.allCountries.push(feature.properties!['name']);
@@ -41,7 +60,39 @@ export class CountryVerificationService {
     return {firstCountry: this.allCountries[firstCountryIdx], secondCountry: this.allCountries[secondCountryIdx]};
   }
 
-  verifyCountry() {
-    
+  verifyCountry(answer: string): boolean {
+    const country: LongLat | undefined = this.countryLongLatData.get(answer);
+    if (country === undefined) return false;
+    const point: Point = {
+      type: 'Point',
+      coordinates: [country.longitude, country.latitude]
+    }
+    for (const feature in this.geojsonData?.features) {
+      
+    }
+    return true;
+  }
+
+  private loadLongLat(): void {
+    fetch('assets/country-coord.csv')
+    .then(response => response.text())
+    .then(csvData => {
+      Papa.parse<CSVRow>(csvData, {
+        header: true,
+        skipEmptyLines: true,
+        complete: (results: { data: CSVRow[]; }) => {
+          results.data.forEach((row: any) => {
+            const country = row['Country'];
+            const longitude = Number(row['Longitude (average)']);
+            const latitude = Number(row['Latitude (average)']);
+            this.countryLongLatData.set(country, { longitude, latitude });
+          });
+        },
+        error: (error: Error) => {
+          console.error('Error parsing CSV:', error);
+        }
+      });
+    })
+    .catch(error => console.error('Error fetching CSV file:', error));
   }
 }
