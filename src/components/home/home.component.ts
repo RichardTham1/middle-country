0090f07c03302477b9327499a3db6b2ea48c5f2a
RichardT15 <134815093+RichardTham1@users.noreply.github.com>
2024-08-20 16:13:11 -0400

    added autocomplete

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -3,44 +3,33 @@ import { CountryVerificationService } from '../../services/country-verification/
 import { CountryPair } from '../../interfaces/country-pair';
 import { FormsModule } from '@angular/forms';
 
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {startWith, map} from 'rxjs/operators';
-import {AsyncPipe} from '@angular/common';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, MatAutocompleteModule, ReactiveFormsModule, AsyncPipe],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-  control = new FormControl('');
-  streets: string[] = ['Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue'];
-  filteredStreets!: Observable<string[]>;
-
+  filteredCountry: string[] = [];
   answer: string = '';
   twoCountries: CountryPair | null = null;
   constructor(public countryVerificationService: CountryVerificationService) {}
 
   ngOnInit(): void {
-    this.filteredStreets = this.control.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
-
     this.countryVerificationService.onInitialized().then(() => {
       this.getTwoCountries();
     });
   }
 
+  updatelist(event: string) {
+    this.filteredCountry = this._filter(event);
+  }
+
   private _filter(value: string): string[] {
     const filterValue = this._normalizeValue(value);
-    console.log(this.filteredStreets)
-    return this.streets.filter(street => this._normalizeValue(street).includes(filterValue));
+    return this.countryVerificationService.allCountries.filter(country => this._normalizeValue(country).startsWith(filterValue));
   }
 
   private _normalizeValue(value: string): string {
