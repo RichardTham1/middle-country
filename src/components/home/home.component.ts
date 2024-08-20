fd279f9d6651c1fbdfc138a878c25c75fbb4b6f0
RichardT15 <134815093+RichardTham1@users.noreply.github.com>
2024-08-20 14:08:39 -0400

    fixed ngModel

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Input, NgModule, OnInit } from '@angular/core';
 import { CountryVerificationService } from '../../services/country-verification/country-verification.service';
 import { CountryPair } from '../../interfaces/country-pair';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
+  answer: string = '';
   twoCountries: CountryPair | null = null;
   constructor(public countryVerificationService: CountryVerificationService) {}
 
@@ -24,6 +27,13 @@ export class HomeComponent implements OnInit {
   }
 
   verifyAnswer() {
+    console.log(this.answer);
+    this.countryVerificationService.verifyCountry(this.answer, this.twoCountries!);
+  }
 
+  @HostListener('keydown', ['$event'])
+  keyHandler(event: KeyboardEvent) {
+    const key = event.key.normalize('NFD').replace(/\p{Diacritic}/gu, '');
+    if (key === 'Enter') this.verifyAnswer();
   }
 }
