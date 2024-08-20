8ba2f86fe4409422b7040928c82401f5ea744d91
Patrick Dang <93892072+PatrickDang285@users.noreply.github.com>
2024-08-20 15:06:27 -0400

    HELP PLEASE. FILTERED STREETS
    salut, autocomplete fonctionne pas. Filtered streets broken. Merci

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -3,23 +3,48 @@ import { CountryVerificationService } from '../../services/country-verification/
 import { CountryPair } from '../../interfaces/country-pair';
 import { FormsModule } from '@angular/forms';
 
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {startWith, map} from 'rxjs/operators';
+import {AsyncPipe} from '@angular/common';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatAutocompleteModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
+  control = new FormControl('');
+  streets: string[] = ['Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue'];
+  filteredStreets!: Observable<string[]>;
+
   answer: string = '';
   twoCountries: CountryPair | null = null;
   constructor(public countryVerificationService: CountryVerificationService) {}
 
   ngOnInit(): void {
+    this.filteredStreets = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
     this.countryVerificationService.onInitialized().then(() => {
       this.getTwoCountries();
     });
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    console.log(this.filteredStreets)
+    return this.streets.filter(street => this._normalizeValue(street).includes(filterValue));
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
   }
 
   getTwoCountries(): void {
